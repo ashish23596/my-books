d54bdd94ad8d5041d0b1ad5d9bb24c9565b8c928
AshishTukaramKolte <AshishTukaramKolte@MONO-HYD-LAP-58>
2022-08-11 18:04:47 +0530

    Add project files.

---
+++ my-books/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace my_books
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddRazorPages();
+            services.AddControllers();
+            services.AddSwaggerGen(
+                c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "my_books", Version = "v1" });
+                });
+            
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "my_books v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
